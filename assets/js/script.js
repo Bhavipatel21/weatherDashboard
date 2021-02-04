ddb0bfbae1dc2d694f9441275a2e063211be7ef9
Bhavika Patel <bhavij28@gmail.com>
2021-02-03 23:23:18 -0500

    added local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,129 +1,130 @@
-var searchButton = document.getElementById('search-button');
-var cityName = document.getElementById('city-name');
-var currentCity = document.getElementById('current-city');
-var temperature = document.getElementById('temperature');
-var humidity = document.getElementById('humidity');
-var windSpeed = document.getElementById('wind-speed');
-var uvIndex = document.getElementById('uv-index');
-var forecastSection = document.getElementById('forecast');
+var searchButton = document.getElementById("search-button");
+var cityName = document.getElementById("city-name");
+var currentCity = document.getElementById("current-city");
+var temperature = document.getElementById("temperature");
+var humidity = document.getElementById("humidity");
+var windSpeed = document.getElementById("wind-speed");
+var uvIndex = document.getElementById("uv-index");
+var forecastSection = document.getElementById("forecast");
 var now = dayjs();
-var currentDate = (now.format("MM/DD/YYYY"));
+var currentDate = now.format("MM/DD/YYYY");
 
-var weatherUrl = 'https://api.openweathermap.org/data/2.5/weather?units=imperial&q='
-var onecallUrl = 'https://api.openweathermap.org/data/2.5/onecall?units=imperial&'
-var uvIndexDataUrl = 'https://api.openweathermap.org/data/2.5/uvi?'
-var weatherImageUrl = 'https://openweathermap.org/img/wn/'
-var key = '&appid=f630e3736b85bb0ac18e2d1af9cbedca'
-
+var weatherUrl ="https://api.openweathermap.org/data/2.5/weather?units=imperial&q=";
+var onecallUrl ="https://api.openweathermap.org/data/2.5/onecall?units=imperial&";
+var uvIndexDataUrl ="https://api.openweathermap.org/data/2.5/uvi?";
+var weatherImageUrl ="https://openweathermap.org/img/wn/";
+var key = "&appid=f630e3736b85bb0ac18e2d1af9cbedca";
 
 function getCurrentWeather() {
-
-  var name = cityName.value
+  var name = cityName.value;
   var currentWeather = weatherUrl + name + key;
 
-  fetch(currentWeather)
+  //current weather 
+    fetch(currentWeather)
     .then(function (response) {
       return response.json();
     })
     .then(function (weatherdata) {
+      console.log("currentweather ---> ", weatherdata);
+      currentCity.textContent = weatherdata.name + " (" + currentDate + ")";
+      console.log("icon--> ", weatherdata.weather[0].icon);
+      console.log("icon--> ", weatherdata.weather[0].description);
 
-      //console.log("currentweather ---> ", weatherdata)
-      currentCity.textContent = weatherdata.name + " (" + currentDate + ")"
-      //console.log("icon--> ", weatherdata.weather[0].icon)
-      //console.log("icon--> ", weatherdata.weather[0].description)
+      var img = document.createElement("img");
+      img.setAttribute(
+        "src",
+        weatherImageUrl + weatherdata.weather[0].icon + "@2x.png"
+      );
+      img.setAttribute("alt", weatherdata.weather[0].description);
+      currentCity.appendChild(img);
 
-      var img = document.createElement('img')
-      img.setAttribute("src", weatherImageUrl + weatherdata.weather[0].icon + "@2x.png");
-      img.setAttribute("alt", weatherdata.weather[0].description)
-      currentCity.appendChild(img)
+      temperature.textContent = "Temperature : " + weatherdata.main.temp + " °F";
+      humidity.textContent = "Humidity : " + weatherdata.main.humidity + " %";
+      windSpeed.textContent = "Wind Speed : " + weatherdata.wind.speed + " mph";
 
-      temperature.textContent = "Temperature : " + weatherdata.main.temp + " °F"
-      humidity.textContent = "Humidity : " + weatherdata.main.humidity + " %"
-      windSpeed.textContent = "Wind Speed : " + weatherdata.wind.speed + " mph"
+      var latValue = weatherdata.coord.lat;
+      var lonValue = weatherdata.coord.lon;
+      var uvdata = onecallUrl + "lat=" + latValue + "&lon=" + lonValue + key;
 
-      var latValue = weatherdata.coord.lat
-      var lonValue = weatherdata.coord.lon
-      //calculate uv index
-      var uvdata = uvIndexDataUrl + 'lat=' + latValue + '&lon=' + lonValue + key
-    
+
+  //uvdata calcultion 
       fetch(uvdata)
         .then(function (response) {
           return response.json();
         })
         .then(function (uvdata) {
-
-          uvIndex.textContent = "UV-Index : "
-          var uvIndexData = uvdata.value
-          var span = document.createElement('span')
-          uvIndex.appendChild(span)
-          span.textContent = uvIndexData
+          
+          uvIndex.textContent = "UV-Index : ";
+          var uvIndexData = uvdata.daily[0].uvi;
+          var span = document.createElement("span");
+          uvIndex.appendChild(span);
+          span.textContent = uvIndexData;
 
           if (uvIndexData <= 2) {
-            span.setAttribute("class", "badge bg-success")
+            span.setAttribute("class", "badge bg-success");
+          } else if (uvIndexData <= 6) {
+            span.setAttribute("class", "badge bg-warning");
+          } else if (uvIndexData > 6) {
+            span.setAttribute("class", "badge bg-danger");
           }
-          else if (uvIndexData <= 6) {
-            span.setAttribute("class", "badge bg-warning")
-          }
-          else if (uvIndexData > 6) {
-            span.setAttribute("class", "badge bg-danger")
-          }
+        });
 
-        })
-      //daily forecast url call 
-      var options = "&exclude=current,minutely,hourly,alerts"
-      var forecastUrl = onecallUrl + 'lat=' + latValue + '&lon=' + lonValue + options + key
-      // console.log("url", forecastUrl)
-
+    //fiveday forcast 
+      var forecastUrl = onecallUrl + "lat=" + latValue + "&lon=" + lonValue + key;
       fetch(forecastUrl)
         .then(function (response) {
-
           return response.json();
         })
         .then(function (data) {
-            
-           forecastSection.textContent="";
+          
+          forecastSection,textContent="";
+          
           for (var i = 1; i < 6; i++) {
 
-            var dailyDate = new Date(data.daily[i].dt * 1000)
-            var forecastDate = dayjs(dailyDate).format('MM/DD/YYYY') 
-            
-            //create column
-            var div1 = document.createElement("col")
-            forecastSection.appendChild(div1)
-            //add card to column 
-            var div2 = document.createElement("div")
-            div2.setAttribute("class", "card card-body bg-info border-dark")
+            var dailyDate = new Date(data.daily[i].dt * 1000);
+            var forecastDate = dayjs(dailyDate).format("MM/DD/YYYY");
+
+            var div1 = document.createElement("col");
+            forecastSection.appendChild(div1);
+
+            var div2 = document.createElement("div");
+            div2.setAttribute("class", "card card-body bg-info border-dark");
             div1.appendChild(div2);
-            //create h6 tag for date
-            var h6 = document.createElement('h6')
-            h6.textContent = forecastDate
-            div2.appendChild(h6)
-            //create img tag for weather icon
-            var img2 = document.createElement('img')
-            img2.setAttribute("src", weatherImageUrl + data.daily[i].weather[0].icon + "@2x.png");
-            img2.setAttribute("alt", data.daily[i].weather[0].description)
-            div2.appendChild(img2)
-            //create p tag for temparature
-            var forecastTemp = data.daily[i].temp.day
-            var ptag = document.createElement("p")
-            div2.appendChild(ptag)
-            ptag.textContent = "Temp:" + forecastTemp + "°F"
+            var h6 = document.createElement("h6");
+            h6.textContent = forecastDate;
+            div2.appendChild(h6);
 
-            //create p tag for humidity
-            var forecastHumidity = data.daily[i].humidity
-            var ptag2 = document.createElement("p")
-            div2.appendChild(ptag2)
-            ptag2.textContent = "Humidity:" + forecastHumidity + "%"
+            var img2 = document.createElement("img");
+            img2.setAttribute(
+              "src",
+              weatherImageUrl + data.daily[i].weather[0].icon + "@2x.png"
+            );
+            img2.setAttribute("alt", data.daily[i].weather[0].description);
+            div2.appendChild(img2);
+
+            var forecastTemp = data.daily[i].temp.day;
+            var ptag = document.createElement("p");
+            div2.appendChild(ptag);
+            ptag.textContent = "Temp:" + forecastTemp + "°F";
+
+            var forecastHumidity = data.daily[i].humidity;
+            var ptag2 = document.createElement("p");
+            div2.appendChild(ptag2);
+            ptag2.textContent = "Humidity:" + forecastHumidity + "%";
           }
-
-        })
+        });
+    
+   //local storage
+        var searchCities = localStorage.getItem("cities");
+          if (searchCities === null) {
+            searchCities = [];
+          } else {
+            searchCities = JSON.parse(searchCities);
+          }
+        searchCities.push(name);
+        var citiNames = JSON.stringify(searchCities);
+        localStorage.setItem("cities", citiNames);
+        
     });
 }
-searchButton.addEventListener('click', getCurrentWeather);
-
-
-
-
-
-
-
+searchButton.addEventListener("click", getCurrentWeather);
